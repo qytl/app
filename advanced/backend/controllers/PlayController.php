7b3376dca81875772b4afdbb15abb407ea0bf95a
hanjialele925 <2268781158@qq.com>
2017-09-29 16:59:57 +0800

    aaa
    git测试

---
+++ advanced/backend/controllers/PlayController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * PlayController.php
+ *
+ * ...
+ *
+ * Copyright (c)2017 http://note.hanfu8.top
+ *
+ * 修改历史
+ * ----------------------------------------
+ * 2017/9/29, 作者:大乐乐, 操作:创建
+ **/
+
+
+namespace backend\controllers;
+
+
+class PlayController
+{
+    public function play()
+        echo "git test";
+}
+
