044131e3e60948a37d856030df9efaf96b5adb55
gtcarry888@163.com <gtcarry888@163.com>
2017-10-08 14:35:14 +0800

    2017年10月8日14:34:59 前台模板

---
+++ laravel54/app/Http/Controllers/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * User: 豆廷强
+ * Website: www.carry888.top
+ * QQ: 178702482
+ * Date: 2017/10/8
+ * Time: 10:20
+ */
+
+namespace App\Http\Controllers;
+
+class AdminController extends Controller{
+    public function Login(){
+        return view("/index/login");
+    }
+}
+
+
+
