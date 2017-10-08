b6c1eee7ae80a0b8527e63beac21738ec0883dd7
gtcarry888@163.com <gtcarry888@163.com>
2017-10-08 16:10:00 +0800

    2017年10月8日16:09:48    前台的修改

--- laravel54/routes/web.php
+++ laravel54/routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+$uri=$_SERVER['REQUEST_URI'];
+$uri=explode('/', $uri);
+if(isset($uri[5])){
+   Route::any('/'.$uri[4].'/'.$uri[5], ucfirst($uri[4]).'Controller@'.$uri[5]);  
+}else{
+ Route::any('/'.$uri[4], ucfirst($uri[4]).'Controller@index');
+ }
+ 
+ Route::get('/', function () {
+     return view('welcome');
 });
