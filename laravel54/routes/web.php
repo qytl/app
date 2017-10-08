775a435cd139322e7a843a92419c44c7ff4a10c2
gtcarry888@163.com <gtcarry888@163.com>
2017-10-08 15:58:28 +0800

    2017年10月8日15:58:12  前台模板更新 原始

--- laravel54/routes/web.php
+++ laravel54/routes/web.php
@@ -10,18 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-$uri=$_SERVER['REQUEST_URI'];
-$uri=explode('/', $uri);
-if(isset($uri[5]))
-{
-    Route::any('/'.$uri[4].'/'.$uri[5], ucfirst($uri[4]).'Controller@'.$uri[5]);
-}else
-{
-    Route::any('/'.$uri[4], ucfirst($uri[4]).'Controller@index');
-}
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
