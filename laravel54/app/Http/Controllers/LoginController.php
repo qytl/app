bf2cdca3aa374f6bcdc7d0af93c7c4f764276043
hanjialele925 <2268781158@qq.com>
2017-10-09 19:16:11 +0800

    用户登录,退出,导航条显示,判断

--- laravel54/app/Http/Controllers/LoginController.php
+++ laravel54/app/Http/Controllers/LoginController.php
@@ -15,43 +15,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
-use yii\web\Request;
 use App\Http\Requests;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    //退出登录
+    public function login_out(Request $request)
+    {
+        //获取
+        //$data = $request->session()->all();
+        //删除
+        $request->session()->flush();
+        echo "<script>alert('退出成功');location.href='?r=login/homepage'</script>";
+    }
+
     //public $enableCsrfValidation = false; //关闭原有的表单验证类
     public function homepage()
     {
-        return view('Login.login');
+        return view('Login.log');
     }
     public function login()
     {
        $log=$_POST;
-      // var_dump($log);die;
+      //var_dump($log);die;
+       // $user = DB::table('user')->select('user_name',$log['name'])->get();
         $user=DB::table('user')->where(['user_name'=>$log['name']])->first();
-      //  print_r($user->user_name);die;
-        if(empty(json_encode($user,true)))
+       //var_dump($user);die;
+        if($user)
         {
-            return view('Login.login');
-        }
-        else{
+
             if($log['pwd']==$user->user_pwd)
             {
                 echo "登录成功！";
-             //  return view('admin.prompt');
+                // 存储数据到session...
+                session(['user' => $log]);
+                var_dump(session('user') );
+                return view('Login.log');
                 //return redirect('路由')->send();
             }
             else
             {
-                return view('Login.login');
+                return view('Login.log');
             }
+
+        }
+        else
+        {
+            echo"<script>alert('用户名不存在,请重新登录!')</script>";
+            return view('Login.log');die;
         }
     }
     public function submit()
     {
         $user_msg=$_POST;
-        $user=DB::table('user')->where(['user_name'=>$log['name']])->first();
+        $user=DB::table('user')->where(['user_name'=>$user_msg['name']])->first();
         if($user)
         {
             return view('Login/sub');
