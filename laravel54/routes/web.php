d19ef98e5886a9687f9dc60aa9b5a41399d4765b
yongjie666 <506003559@qq,com>
2017-10-09 15:48:13 +0800

    路由配置 优化 2017年10月9日15:48:02

--- laravel54/routes/web.php
+++ laravel54/routes/web.php
@@ -10,14 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-$uri=$_SERVER['REQUEST_URI'];
-$uri=explode('/', $uri);
-if(isset($uri[3])){
-   Route::any('/'.$uri[2].'/'.$uri[3], ucfirst($uri[2]).'Controller@'.$uri[3]);  
-}else{
- Route::any('/'.$uri[2], ucfirst($uri[2]).'Controller@index');
- }
- 
- Route::get('/', function () {
-     return view('welcome');
-});
+
+//路由配置规则
+$ROUTES=isset($_GET['r'])?$_GET['r']:false;
+    if($ROUTES){
+        $ROUTES=explode('/',$ROUTES);
+        Route::match(['get','psot'],'/',ucfirst($ROUTES[0]).'Controller@'.$ROUTES[1]);
+    }else{
+        $uri=$_SERVER['REQUEST_URI'];
+        $uri=explode('/', $uri);
+        if(isset($uri[3])){
+       Route::any('/'.$uri[2].'/'.$uri[3], ucfirst($uri[2]).'Controller@'.$uri[3]);
+    }else{
+         Route::any('/','IndexController@index');
+        }
+    };
